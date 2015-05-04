6619964905150098ea8dd2d6db233d20cb389e49
Kacper Trafial <kacper.trafial@live.com>
2015-05-04 23:14:45 +0200

    Pobrawione błędy i dodane nowe elementy designu

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -3,5 +3,15 @@
  */
 
 $(document).ready(function(){
+    $('.JumboFinger').click(function(){
+        $.fn.fullpage.moveTo(2, 1);
+    });
 
+    $('.JumboFace').click(function(){
+        $.fn.fullpage.moveTo(2, 2);
+    });
+
+    $('.JumboZiom').click(function(){
+        $.fn.fullpage.moveTo(2, 3);
+    });
 });
